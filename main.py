805ede1ebcfd7ae573d16efc24f4231f10b9a874
Honza66 <luiggi@seznam.cz>
2024-04-22 20:19:55 +0200

    vytvoření fce get_hash_from_database

--- main.py
+++ main.py
@@ -15,4 +15,24 @@ def insert_user(email, password):
     connection.commit()
     connection.close()
 
-insert_user('homer@sexymeda.com', 'admin')
+def get_hash_from_database(email):
+
+    connection = psycopg2.connect(
+        dbname = 'bank',
+        user = 'postgres',
+        password = 'admin',
+        host= 'localhost',
+        port = '5432'
+    )
+    cur = connection.cursor()
+    query = ''' SELECT password FROM bank_user WHERE email = (%s)'''
+    cur.execute(query, (email,))
+    user_hash_password = cur.fetchone()
+    connection.close()
+    if user_hash_password:
+        return bytes.fromhex(user_hash_password[0][2:])
+    else:
+        return b''
+
+    
+
