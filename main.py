aa2540298f72ce3a8796058b316f7303f5de89e0
Honza66 <luiggi@seznam.cz>
2024-04-26 21:15:12 +0200

    zachytávání chyb

--- main.py
+++ main.py
@@ -5,56 +5,76 @@ from tkinter import *
 
 
 def password_to_hash(plain_password):
-    password_bytes = plain_password.encode('utf-8')
-    hash = bcrypt.hashpw(password_bytes, bcrypt.gensalt())
-    return hash
+    try:
+        password_bytes = plain_password.encode('utf-8')
+        hash = bcrypt.hashpw(password_bytes, bcrypt.gensalt())
+        return hash
+    except Exception as error:
+        print(f'Chyba při hashování hesla {error}')
+
 
 
 def insert_user(email, password):
-    connection = psycopg2.connect(
+    try:
+        connection = psycopg2.connect(
         dbname = 'bank',
         user = 'postgres',
         password = 'admin',
         host= 'localhost',
         port = '5432'
-    )
-    cur = connection.cursor()
-    query = ''' INSERT INTO bank_user(email, password) VALUES(%s, %s)'''
-    hash = password_to_hash(password)
-    cur.execute(query, (email, hash))
-    connection.commit()
-    connection.close()
+        )
+        cur = connection.cursor()
+        query = ''' INSERT INTO bank_user(email, password) VALUES(%s, %s)'''
+        hash = password_to_hash(password)
+        cur.execute(query, (email, hash))
+        connection.commit()
+        connection.close()
+    except psycopg2.DatabaseError as error:
+        print(f'Chyba databáze {error}')
+    except Exception as error:
+        print(f'obecná chyba {error}')
 
 
 def get_hash_from_database(email):
+    try:
 
-    connection = psycopg2.connect(
+        connection = psycopg2.connect(
         dbname = 'bank',
         user = 'postgres',
         password = 'admin',
         host= 'localhost',
         port = '5432'
-    )
-    cur = connection.cursor()
-    query = ''' SELECT password FROM bank_user WHERE email = (%s)'''
-    cur.execute(query, (email,))
-    user_hash_password = cur.fetchone()
-    connection.close()
-    if user_hash_password:
-        return bytes.fromhex(user_hash_password[0][2:])
-    else:
+        )
+        cur = connection.cursor()
+        query = ''' SELECT password FROM bank_user WHERE email = (%s)'''
+        cur.execute(query, (email,))
+        user_hash_password = cur.fetchone()
+        connection.close()
+        if user_hash_password:
+            return bytes.fromhex(user_hash_password[0][2:])
+        else:
+            return b''
+    except psycopg2.DatabaseError as error:
+        print(f'Chyba databáze {error}')
+        return b''
+    except Exception as error:
+        print(f'obecná chyba {error}')
         return b''
     
 def login_autentication(password,email):
-    hash = get_hash_from_database(email)
-    password_byte = bytes(password, encoding='utf-8')
-    if hash == b'':
-        result_label['text'] = 'Neplatné'
-    else:
-        if bcrypt.checkpw(password_byte, hash):
-            result_label['text'] = 'Úspěšné přihlášení'
-        else:
+    try:
+        hash = get_hash_from_database(email)
+        password_byte = bytes(password, encoding='utf-8')
+        if hash == b'':
             result_label['text'] = 'Neplatné'
+        else:
+            if bcrypt.checkpw(password_byte, hash):
+                result_label['text'] = 'Úspěšné přihlášení'
+            else:
+                result_label['text'] = 'Neplatné'
+    except Exception as error:
+        result_label['text'] = 'Chyba při ověřování'
+        print(f'Chyba při ověřování {error}')
             
 root = Tk()
 root.title('Registrace a přihlášení')
